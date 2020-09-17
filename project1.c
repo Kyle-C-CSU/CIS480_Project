f3424509ec07d0e652fe280d4c1dcc559e1ecb91
Kyle-C-CSU <71400517+Kyle-C-CSU@users.noreply.github.com>
2020-09-17 00:19:35 -0400

    Update project1.c
    1. Created switch feature 
    2. Added todo list in main 
    3. Created push array method (needs work)

--- project1.c
+++ project1.c
@@ -31,14 +31,72 @@ int *decimalToBinary_n(int dec, int *binMSB, int n){
     return binMSB;
 }
 
+//push values from a to b 
+int *arrypush(int *a, int *b, int offset, int n ){
+    for(int i=offset;i<5;i++){
+        b[i] = a[i];
+    }
+    return b;
+}
+
+//instruction convertor 
+int ins_conv(char *ins){
+    char *tab[] = {"ADD","SUB","SLL","SRL","SLT" //symbol table 
+                   "ADDI","LUI","ORI","LW","SW"};
+    int index;
+    upper(ins);
+    
+    for(int i = 0;i<10;i++){
+        if(strcmp(tab[i],ins)==0){
+            index = ++i;
+            break;
+        }
+    }
+    
+    return index;
+}
+
 
 
 int main(int argc, char *argv[]) {
 
-   FILE *fp; //pointer for read file 
-   char str[60],label[20], instruction[20], operand[20]; //buffer vals 
-   bool data_sec; 
-   int bitcount, data[512], text[512]; //binary buffer vals 
+
+
+
+    /*To do:
+    *1. find solution to identify labels and store accordingly 
+    *2. find solution to reference labels (symbol table)
+    *3. fix arraypush method: not storing correct values by reference
+    *4. finish .data condition   
+    *5. Finish formating strings for each instruction case
+    *6. implement LA instruction 
+    *7. Create 1024 array to store data + txt feilds and find a solution to concatinate
+    */
+
+    /*Bugs:
+    * Segmentation fault because instruction is being read into label 
+    * then trying to pass gibberish to method that is expecting string 
+    * once 1 is taken care of on to do list. This should be managed. 
+
+    */
+
+
+
+
+
+    FILE *fp; //pointer for read file 
+    char str[60],lab[20], ins[20], op[20]; //buffer vals 
+    bool data_sec; 
+    int bitcount, opcode,rd,rs,rt,imm, data[512], text[512]; //binary buffer vals 
+
+
+    
+    int reg6[] = {0,0,0,0,0,0};
+    int reg5[] = {0,0,0,0,0,0};
+    for(int i = 0; i < 512; i++)
+        data[i]=0;
+
+
 
    /* opening file for reading */
     fp = fopen(argv[1] , "r");
@@ -54,15 +112,19 @@ int main(int argc, char *argv[]) {
    /*get line by line of read file until EOF*/
     while( fgets (str, 60, fp)!=NULL ) {
         /* print content to stdout */
+        int len = strlen(str); 
+        //trim off the /n
+        if( str[len-1] == '\n' ) 
+            str[len-1] = 0;
         puts(str);
 
-    
-        if (strcmp(str, ".data")==0){
+        
+        if (strcmp(str, "\t.data")==0){
             data_sec = true;
             bitcount = 0;
 
         }
-        if(strcmp(str, ".text")==0){
+        if(strcmp(str, "\t.text")==0){
             data_sec = false;
             bitcount = 0;
         }
@@ -70,16 +132,84 @@ int main(int argc, char *argv[]) {
         
         if(data_sec){
         /*.data section first 512 bits*/
-            sscanf( str, "%s %s %s", label, instruction, operand );
-            //read all data section into string
-            //parse into op, rs, rt, rd, shamt, func
-            //convert into binary
-            //append all remaing bits to 0
+            sscanf( str, "%s %s %s", lab, ins, op );
         }
 
         else{
         /*.text section last 512 bits*/
-            sscanf( str, "%s\t%s\t%s\n", label, instruction, operand );
+            sscanf( str, "%s %s %s", lab, ins, op );
+            //check for label error handeling temporary
+            int input;
+            if(strlen(ins)<3)
+                input = -1;
+            else
+                input = ins_conv(ins);
+                
+            
+            switch(input){
+                case 1: //add
+                    sscanf( op, "$%d,$%d,$%d", &rd, &rs, &rt );
+                    opcode = 0; 
+                    decimalToBinary_n(opcode, reg6, 6);
+                    arrypush(reg6,data,bitcount,6);
+                    bitcount+=6;
+                    decimalToBinary_n(rs, reg5, 5);
+                    arrypush(reg5,data,bitcount,5);
+                    bitcount+=5;
+                    decimalToBinary_n(rt, reg5, 5);
+                    arrypush(reg5,data,bitcount,5);
+                    bitcount+=5;
+                    decimalToBinary_n(rd, reg5, 5);
+                    arrypush(reg5,data,bitcount,5);
+                    bitcount+=5;
+                    decimalToBinary_n(0,reg5,5);
+                    arrypush(reg5,data,bitcount,5);
+                    bitcount+=5;
+                    decimalToBinary_n(32, reg6, 6);
+                    arrypush(reg6, data,bitcount,6);
+                    bitcount+=6; 
+                    break;
+                    
+                case 2: //sub
+                    sscanf( op, "$%d,$%d,$%d", &rd, &rs, &rt );
+                    opcode = 0;
+                    break;
+                case 3: //sll
+                    sscanf( op, "$%d,$%d,%d", &rd, &rs, &rt );
+                    opcode = 0;
+                    break;
+                case 4: //srl
+                    sscanf( op, "$%d,$%d,%d", &rd, &rs, &rt );
+                    opcode = 0;
+                    break;
+                case 5: //slt
+                    sscanf( op, "$%d,$%d,$%d", &rd, &rs, &rt );
+                    opcode = 0;
+                    break;
+                case 6: //addi
+                    sscanf( op, "$%d,$%d,%d", &rd, &rs, &rt );
+                    opcode = 8;
+                    break;
+                case 7: //lui
+                    sscanf( op, "$%d,%s", &rd, lab);
+                    opcode = 15;
+                    break;
+                case 8: //ori
+                    sscanf( op, "$%d,$%d,%s", &rd,&rs,lab);
+                    opcode = 13;
+                    break;
+                case 9: //lw
+                    sscanf( op, "$%d,%d($%d)", &rd, &imm, &rs);
+                    opcode = 35;
+                    break;
+                case 10: //sw
+                    sscanf( op, "$%d,%d($%d)", &rd, &imm, &rs);
+                    opcode = 43;
+                    break;
+                     
+                default:
+                    printf("Error: value passed not between 1-10\n");
+            }
             //read all text section into string
             //parse into op, rs, rt, rd, shamt, func
             //op
@@ -87,8 +217,8 @@ int main(int argc, char *argv[]) {
             //convert into binary
             //append all remaing bits to 0
         }
+            
         /*scan data into place holders for bin data to be converted*/
-       
         /*write binary to new file(binfile)*/
         if (fwrite(str,sizeof(str), 1, fpnew )!=1) {
             perror("Error writing file"); return(-1);
